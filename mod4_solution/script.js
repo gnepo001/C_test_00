8a89fb1745278eb362f18a839a8201de2dc9f469
gnepo001 <gilbertnepomuceno99@gmail.com>
2021-01-08 09:31:57 -0800

    console Javascript program

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -26,7 +26,6 @@ as described in the requirements and the steps in order to complete this
 assignment.
 WARNING!!! WARNING!!!
 */
-
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
